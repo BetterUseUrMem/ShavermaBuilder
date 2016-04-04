c58972d98a89edb6683acb31214a3c79def648ed
BetterUseUrMem <PoccoJIu4@gmail.com>
2016-04-04 14:12:24 +0700

    Added files via upload

---
+++ hovanometr.h
@@ -0,0 +1,29 @@
+namespace
+{
+	enum lavash_type
+	{
+		Myagkiy,
+		Tverdyu,
+		Hrystashyu
+	};
+
+	enum sous_type
+	{
+		chesnochnuy,
+		ostriu,
+		firmennuy,
+		tomatnyu,
+		sirnuy
+	};
+
+	struct Shaverma
+	{
+		int pomidor;
+		int myaso;
+		int kapusta;
+		int kartoshka;
+		int morkov_koryaskaya;
+		sous_type sous;
+		lavash_type lavash;
+	};
+}
