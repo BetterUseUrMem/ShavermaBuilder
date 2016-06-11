5529a2c56e5d04f739b77467cbc558ff3c76fd13
BetterUseUrMem <PoccoJIu4@gmail.com>
2016-06-11 16:16:34 +0700

    новый хованометр

--- hovanometr.hpp
+++ hovanometr.hpp
@@ -1,38 +1,56 @@
-#include <iostream>
-#include <Windows.h>
-namespace WAYRMA
-{
-	enum lavash_type
-	{
-		Myagkiy,
-		Tverdyu,
-		Hrystashyu
-	};
+namespace hh {
 
 	enum sous_type
 	{
 		chesnochnuy,
 		ostriu,
-		firmennuy,
 		tomatnyu,
 		sirnuy
 	};
 
-	struct Shaverma
-	{
+	struct shaverstruct {
 		int pomidor;
 		int myaso;
 		int kapusta;
 		int kartoshka;
 		int morkov_koryaskaya;
 		sous_type sous;
-		lavash_type lavash;
 	};
 
-	void PRIGOTOVIT
-	{
-		std::cout << "Shaverma gorovitsya..." << std::endl;
-	    Sleep(50000);
-		std::cout << "Gotovo!!! BON APPETI" << std::endl;
-	}
+	class ShavermBuilder {
+	private:
+		float c;
+	public:
+		void Calc(shaverstruct *s) {
+			if (s != 0x0) {
+				int sous{};
+				switch (s->sous)
+				{
+				case chesnochnuy:
+					sous = 335;
+					break;
+				case ostriu:
+					sous = 11;
+					break;
+				case tomatnyu:
+					sous = 29;
+					break;
+				case sirnuy:
+					sous = 356;
+					break;
+				default:
+					sous = 335;
+					break;
+				}
+
+				c = ((s->kapusta/100) * 27) + ((s->kartoshka/100) * 77) + ((s->morkov_koryaskaya/100) * 134)
+					+ ((s->myaso/100) * 110) + ((s->pomidor/100) * 18) + sous;
+			}
+		}
+
+		float GetCalories() {
+			return c;
+		}
+
+	};
 }
